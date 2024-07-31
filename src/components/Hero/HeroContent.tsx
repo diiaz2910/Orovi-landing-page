39c46932faf568744937293b2e1c38a9f99942dd
Ricardo <diiaz2910@gmail.com>
2024-07-31 22:01:18 +1200

    Hero section added to the home page

---
+++ src/components/Hero/HeroContent.tsx
@@ -0,0 +1,33 @@
+import { Overlay, Container, Title, Button, Text } from "@mantine/core";
+import classes from "./HeroContent.module.css";
+
+export function HeroContent() {
+  return (
+    <div className={classes.hero}>
+      <Overlay
+        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+        opacity={1}
+        zIndex={0}
+      />
+      <Container className={classes.container} size="md">
+        <Title className={classes.title}>
+          A green paradise in the heart of Colombia
+        </Title>
+        <Text className={classes.description} size="xl" mt="xl">
+          Come to visit the most beautiful places in Colombia and enjoy your
+          stay with us at OROVI Finca Hotel. We offer the best service and the
+          most comfortable rooms for you to relax and enjoy your holidays.
+        </Text>
+
+        <Button
+          variant="gradient"
+          size="xl"
+          radius="xl"
+          className={classes.control}
+        >
+          Discover
+        </Button>
+      </Container>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import { HeaderMenu } from "../components/Header/HeaderMenu";
 import { LeadGrid } from "../components/Grids/Grids";
 import { FooterCentered } from "../components/Footer/Footer";
+import { HeroContent } from "../components/Hero/HeroContent";
 
 const Home: React.FC = () => {
   return (
     <div>
       <HeaderMenu />
+      <HeroContent />
       <div>
         <h1>Home Page</h1>
       </div>
